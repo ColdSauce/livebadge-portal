8d056ac0748b2222836db102831b06fa99c98b19
Patrick Pistor <yogert96@gmail.com>
2018-03-14 04:45:59 -0500

    More styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as FontAwesome from 'react-icons/lib/fa'
+
 class Home extends Component {
 	render() {
     return(
-	    <div className="app">
-	        <div className="container">
-				<div className="box" id='topleft'>
-					<a href="manage"> Manage Event </a>
-				</div>
-				<div className="box" id='topright'>
-					<a href="addConcert"> Add Concert </a>
-				</div><br /><br />
-				<div className="box" id='bottomleft'>
-					<a href="addPromotion"> Add Promotion </a>
-				</div>
-				<div className="box" id='bottomright'>
-					<a href="social"> View Social Reaction </a>
-				</div>
-	        </div>
+    	<div className="">
+	    	<a href="/"><FontAwesome.FaCog className="cog"/></a>
+		    <div className="app">
+		        <div className="container">
+					<a href="manage"><div className="box" id='topleft'>
+						 Manage Event 
+					</div></a>
+					<a href="addConcert"><div className="box" id='topright'>
+						 Add Concert 
+					</div></a><br /><br />
+					<a href="addPromotion"><div className="box" id='bottomleft'>
+						 Add Promotion 
+					</div></a>
+					<a href="social"><div className="box" id='bottomright'>
+						 View Social Reaction 
+					</div></a>
+		        </div>
+		    </div>
 	    </div>
 	  )
 	};  
