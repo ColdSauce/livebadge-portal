4327b9a9bbfeb2a6de9b0eacc7198bc91dc3482d
Stefan Aleksic <stefan@stefanaleksic.com>
2018-03-13 21:42:12 -0400

    Get static file serving working.

--- server/main.go
+++ server/main.go
@@ -1,17 +1,22 @@
 package main
 
-import "github.com/coldsauce/livebadge-portal/auth"
+import "github.com/coldsauce/livebadge-portal/server/auth"
 
 // import "github.com/coldsauce/livebadge-portal/api"
 import "net/http"
+import "github.com/gorilla/mux"
+import "github.com/urfave/negroni"
 
 // main creates and starts a Server listening.
 func main() {
-	mux := http.NewServeMux()
-
+	r := mux.NewRouter()
 	address := "localhost:8080"
-	mux = auth.InjectAuth(mux)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/build"))))
+	dir := "./_frontend/build/"
+	r = auth.InjectAuth(r)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(dir)))
 
-	http.ListenAndServe(address, mux)
+	n := negroni.Classic() // Includes some default middlewares
+	n.UseHandler(r)
+
+	http.ListenAndServe(address, n)
 }
